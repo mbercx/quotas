cd4c9cae3e31554fedd9eb6dd70c11176062783b
Marnik Bercx <mbercx@gmail.com>
2017-09-28 10:36:13 +0200

    CLI improvements

--- quotas/cli/commands/setup.py
+++ quotas/cli/commands/setup.py
@@ -1,7 +1,13 @@
 import pymatgen as pmg
 
-def slab_setup(filename):
+def slab_setup(filename, miller_indices, thickness, ):
     """
+    Set up all the calculations to study the slab of a structure.
+
+    Args:
+        filename (string): Name of the structure file.
+        miller_indices (list): The miller indices of the surface.
+
 
     Returns:
 
--- quotas/cli/quotas.py
+++ quotas/cli/quotas.py
@@ -18,13 +18,27 @@ def main():
     """
     pass
 
-@main.command(context_settings=CONTEXT_SETTINGS)
-@click.argument("filename")
+@main.group(context_settings=CONTEXT_SETTINGS)
 def setup(filename):
     """
-    Set up all the calculations for the input required for the QUOTAS model,
-    based on the bulk structure.
+    Set up all calculations for the input required in the QUOTAS model.
+    """
+    pass
+
+@setup.command(context_settings=CONTEXT_SETTINGS)
+@click.argument("miller_indices")
+@click.argument("filename")
+@click.option("--vacuum", "-v", default=15,
+              help="Minimum thickness of the vacuum layer.")
+@click.option("--thickness", "-t", default=20,
+              help="Minimum thickness of the slab, in Angstroms.")
+@click.option("--fix", "-f", default="center",
+              help="Part of the slab to fix in the geometry optimization.")
+def slab(miller_indices, filename):
+    """
+    Set up all the calculations for a specific surface of a structure.
     """
     from quotas.cli.commands.setup import slab_setup
 
-    slab_setup(filename=filename)
+    slab_setup(filename=filename,
+               miller_indices=miller_indices)
