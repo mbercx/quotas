130885f6d8046cb4c276f9c879cb825e9d841e2c
Marnik Bercx <mbercx@gmail.com>
2017-11-27 14:36:52 +0100

    Test: Try to fix the CHGCAR copying. I'm running into issues loading the CHGCAR in VASP.

--- quotas/cli/commands/bulk.py
+++ quotas/cli/commands/bulk.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import subprocess
 
 from quotas.calculation import bulkRelaxSet, bulkDosSet, bulkDosHSESet
 
@@ -98,8 +99,10 @@ def dos(relax_dir, k_product, hse_calc=False):
     if not hse_calc:
 
         # Copy the charge density from the geometry optimization
-        shutil.copy(os.path.join(relax_dir, "CHGCAR"),
-                    os.path.join(calculation_dir))
+        # shutil.copy(os.path.join(relax_dir, "CHGCAR"),
+        #             os.path.join(calculation_dir))
+        subprocess.call(["cp", os.path.join(relax_dir, "CHGCAR"),
+                         os.path.join(calculation_dir)])
 
 def diel():
     pass
