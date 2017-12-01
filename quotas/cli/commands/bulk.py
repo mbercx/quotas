e90ea1bf3281972f3b519c659942c052e9cef19c
Marnik Bercx <mbercx@gmail.com>
2017-12-01 21:45:19 +0100

    test

--- quotas/cli/commands/bulk.py
+++ quotas/cli/commands/bulk.py
@@ -159,6 +159,8 @@ def diel(relax_dir, k_product, hse_calc, is_metal, verbose):
     if verbose:
         print("Setting up calculation...")
 
+    print(user_incar_settings)
+
     diel_calculation = bulkSCFSet.from_relax_calc(
         relax_dir=relax_dir,
         k_product=k_product,
