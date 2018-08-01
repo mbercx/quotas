10f2ec97436205f52407fbd6c8dea905e99540d7
Marnik Bercx <mbercx@gmail.com>
2018-08-01 14:35:13 +0200

    Added WorkFunctionData class, in order to try to store wf data more compactly.

--- quotas/core.py
+++ quotas/core.py
@@ -264,9 +264,215 @@ def generate_divisors(number):
         divisor -= 1
 
 
-# Stolen from pymatgen.analysis.surface_analyzer
+# Stolen from pymatgen.analysis.surface_analysis
 # Idea is to write a nice postprocessing tool extracting the necessary
 # output from the work function and DOS calculations.
+
+class WorkFunctionData(MSONable):
+    """
+    A class that contains the necessary data to calculate the work function
+    of a slab, as well as plot the average local potential along the line
+    perpendicular to the surface.
+
+    Shamelessly copied from pymatgen.analysis.surface_analysis, in order to
+    create a more lean class which I can store in a json file and push to
+    the dataserver.
+
+    """
+    def __init__(self, poscar, locpot_along_c, efermi, shift=0):
+        """
+        Initializes the WorkFunctionData class.
+
+        Args:
+            poscar (pymatgen.io.vasp.outputs.Poscar): Poscar vasp output object
+            locpot_along_c (numpy.ndarray): Average potential along the c axis.
+            efermi (float): Fermi level, i.e. the energy of the highest
+                occupied state.
+            shift (float): Parameter to translate the slab (and
+                therefore the vacuum) of the slab structure, thereby
+                translating the plot along the x axis.
+        """
+
+        self.poscar = poscar
+
+        # properties that can be shifted
+        slab = poscar.structure.copy()
+        slab.translate_sites([i for i, site in enumerate(slab)], [0, 0, shift])
+        self.slab = slab
+        self.sorted_sites = sorted(self.slab,
+                                   key=lambda site: site.frac_coords[2])
+
+        # Get the plot points between 0 and c
+        # increments of the number of locpot points
+        self.along_c = np.linspace(0, 1, num=len(locpot_along_c))
+        locpot_along_c_mid, locpot_end, locpot_start = [], [], []
+        for i, s in enumerate(self.along_c):
+            j = s + shift
+            if j > 1:
+                locpot_start.append(locpot_along_c[i])
+            elif j < 0:
+                locpot_end.append(locpot_along_c[i])
+            else:
+                locpot_along_c_mid.append(locpot_along_c[i])
+        self.locpot_along_c = locpot_start + locpot_along_c_mid + locpot_end
+
+        # get the average of the signal in the bulk-like region of the
+        # slab, i.e. the average of the oscillating region. This gives
+        # a rough appr. of the potential in the interior of the slab
+        bulk_p = [p for i, p in enumerate(self.locpot_along_c) if \
+                  self.sorted_sites[-1].frac_coords[2] > self.along_c[i] \
+                  > self.sorted_sites[0].frac_coords[2]]
+        self.ave_bulk_p = np.mean(bulk_p)
+
+        # shift independent quantities
+        self.efermi = efermi
+        self.vacuum_locpot = max(self.locpot_along_c)
+        # get the work function
+        self.work_function = self.vacuum_locpot - self.efermi
+        # for setting ylim and annotating
+        self.ave_locpot = (self.vacuum_locpot - min(self.locpot_along_c)) / 2
+
+    def get_locpot_along_slab_plot(self, label_energies=True, plt=None,
+                                   label_fontsize=10):
+        """
+        Returns a plot of the local potential (eV) vs the
+            position along the c axis of the slab model (Ang)
+
+        Args:
+            label_energies (bool): Whether to label relevant energy
+                quantities such as the work function, Fermi energy,
+                vacuum locpot, bulk-like locpot
+            plt (plt): Matplotlib pylab object
+            label_fontsize (float): Fontsize of labels
+
+        Returns plt of the locpot vs c axis
+        """
+
+        plt = pretty_plot() if not plt else plt
+        ax = list(plt.subplots())[1]
+        ax.spines['top'].set_visible(False)
+
+        # plot the raw locpot signal along c
+        plt.plot(self.along_c, self.locpot_along_c, 'b--')
+
+        # Get the local averaged signal of the locpot along c
+        xg, yg = [], []
+        for i, p in enumerate(self.locpot_along_c):
+            # average signal is just the bulk-like potential when in the slab region
+            if p < self.ave_bulk_p \
+                    or self.sorted_sites[-1].frac_coords[2] >= self.along_c[i] \
+                            >= self.sorted_sites[0].frac_coords[2]:
+                yg.append(self.ave_bulk_p)
+                xg.append(self.along_c[i])
+            else:
+                yg.append(p)
+                xg.append(self.along_c[i])
+        xg, yg = zip(*sorted(zip(xg, yg)))
+        plt.plot(xg, yg, 'r', linewidth=2.5, zorder=-1)
+
+        # make it look nice
+        if label_energies:
+            plt = self.get_labels(plt, label_fontsize=label_fontsize)
+        plt.xlim([0, 1])
+        plt.ylim([min(self.locpot_along_c),
+                  self.vacuum_locpot + self.ave_locpot * 0.2])
+        plt.xlabel(r"Fractional coordinates ($\hat{c}$)", fontsize=25)
+        plt.xticks(fontsize=15, rotation=45)
+        plt.ylabel(r"Energy (eV)", fontsize=25)
+        plt.yticks(fontsize=15)
+
+        return plt
+
+    def get_labels(self, plt, label_fontsize=10):
+        """
+        Handles the optional labelling of the plot with relevant quantities
+        Args:
+            plt (plt): Plot of the locpot vs c axis
+            label_fontsize (float): Fontsize of labels
+        Returns Labelled plt
+        """
+
+        maxc = self.sorted_sites[-1].frac_coords[2]
+        minc = self.sorted_sites[0].frac_coords[2]
+        # determine whether most of the vacuum is to
+        # the left or right for labelling purposes
+        vleft = [i for i in self.along_c if i <= minc]
+        vright = [i for i in self.along_c if i >= maxc]
+        if max(vleft) - min(vleft) > max(vright) - min(vright):
+            label_in_vac = (max(vleft) - min(vleft)) / 2
+        else:
+            label_in_vac = (max(vright) - min(vright)) / 2 + maxc
+
+        # label the vacuum locpot
+        label_in_bulk = maxc - (maxc - minc) / 2
+        plt.plot([0, 1], [self.vacuum_locpot] * 2, 'b--', zorder=-5,
+                 linewidth=1)
+        xy = [label_in_bulk, self.vacuum_locpot + self.ave_locpot * 0.05]
+        plt.annotate(r"$V_{vac}=%.2f$" % (self.vacuum_locpot), xy=xy,
+                     xytext=xy, color='b', fontsize=label_fontsize)
+
+        # label the fermi energy
+        plt.plot([0, 1], [self.efermi] * 2, 'g--',
+                 zorder=-5, linewidth=3)
+        xy = [label_in_bulk, self.efermi + self.ave_locpot * 0.05]
+        plt.annotate(r"$E_F=%.2f$" % (self.efermi), xytext=xy,
+                     xy=xy, fontsize=label_fontsize, color='g')
+
+        # label the bulk-like locpot
+        plt.plot([0, 1], [self.ave_bulk_p] * 2, 'r--', linewidth=1., zorder=-1)
+        print(label_in_vac)
+        xy = [label_in_vac, self.ave_bulk_p + self.ave_locpot * 0.05]
+        plt.annotate(r"$V^{interior}_{slab}=%.2f$" % (self.ave_bulk_p),
+                     xy=xy, xytext=xy, color='r', fontsize=label_fontsize)
+
+        # label the work function as a barrier
+        plt.plot([label_in_vac] * 2, [self.efermi, self.vacuum_locpot],
+                 'k--', zorder=-5, linewidth=2)
+        xy = [label_in_vac, self.efermi + self.ave_locpot * 0.05]
+        plt.annotate(r"$\Phi=%.2f$" % (self.work_function),
+                     xy=xy, xytext=xy, fontsize=label_fontsize)
+
+        return plt
+
+    @staticmethod
+    def from_files(poscar_filename, locpot_filename, outcar_filename, shift=0):
+        poscar = Poscar.from_file(poscar_filename)
+        locpot = Locpot.from_file(locpot_filename)
+        outcar = Outcar(outcar_filename)
+
+        wf = WorkFunctionAnalyzer(poscar, locpot, outcar, shift=shift)
+
+        return WorkFunctionData(poscar, np.array(wf.locpot_along_c),
+                                wf.efermi, shift=shift)
+
+    def as_dict(self):
+        """
+
+        Returns:
+
+        """
+
+        d = {"@module": self.__class__.__module__,
+             "@class": self.__class__.__name__,
+             "poscar": self.poscar.as_dict(),
+             "locpot_along_c": self.locpot_along_c,
+             "efermi": self.efermi}
+
+        return d
+
+    def from_dict(cls, d):
+        """
+
+        Args:
+            d:
+
+        Returns:
+
+        """
+
+        return cls.__init__(Poscar.from_dict(d["poscar"]),
+                            d["locpot_along_c"],
+                            d["efermi"])
 
 class WorkFunctionAnalyzer(MSONable):
     """
